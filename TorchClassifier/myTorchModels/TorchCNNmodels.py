aff65c88d8dcc7bce180c99b515ed17b2f8fe9ef
Sivasaikrishna19 <146486981+Sivasaikrishna19@users.noreply.github.com>
2024-05-03 22:39:30 -0700

    Update TorchCNNmodels.py VGG

--- TorchClassifier/myTorchModels/TorchCNNmodels.py
+++ TorchClassifier/myTorchModels/TorchCNNmodels.py
@@ -92,27 +92,22 @@ def createTorchCNNmodel(name, numclasses, img_shape, pretrained=True):
         #return create_torchvisionmodel(name, numclasses, pretrained)
         return create_torchvisionmodel(name, numclasses, freezeparameters=True, pretrained=pretrained)
 
-def create_vggmodel1(numclasses, img_shape):
-    # Load the pretrained model from pytorch
-    vgg16 = models.vgg16(pretrained=True)
+def create_vggmodel1(num_classes):
+    # Load the pretrained VGG16 model
+    weights = VGG16_Weights.DEFAULT
+    vgg16 = models.vgg16(weights=weights)
 
-    # print out the model structure
-    print(vgg16)
-    print(vgg16.classifier[6].in_features) #4096
-    print(vgg16.classifier[6].out_features) #1000
-
-    #Freeze training for all "features" layers
+    # Freeze training for all "features" layers
     for param in vgg16.features.parameters():
         param.requires_grad = False
-    
-    #Final Classifier Layer, Once you have the pre-trained feature extractor, you just need to modify and/or add to the final, fully-connected classifier layers. Replace the last layer in the vgg classifier group of layers.
+
+    # Replace the last layer in the VGG classifier group of layers
     n_inputs = vgg16.classifier[6].in_features
+    vgg16.classifier[6] = nn.Linear(n_inputs, num_classes)
 
-    # add last linear layer (n_inputs -> 5 flower classes)
-    # new layers automatically have requires_grad = True
-    last_layer = nn.Linear(n_inputs, numclasses)
+    print(vgg16)
 
-    vgg16.classifier[6] = last_layer
+    return vgg16
     
 class VGG(nn.Module):
     def __init__(self, features, output_dim):
@@ -139,79 +134,42 @@ class VGG(nn.Module):
         x = self.classifier(h)
         return x, h
 
-def create_vggcustommodel(numclasses, img_shape):
-    #Each item in the list is either 'M', which denotes a max pooling layer, or an integer, which denotes a convolutional layer with that many filters.
-    vgg11_config = [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
-
-    vgg13_config = [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
-
-    vgg16_config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 
-                    512, 'M']
-
-    vgg19_config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 
-                    512, 512, 512, 512, 'M']
-
-    vgg11_layers = get_vgg_layers(vgg11_config, batch_norm = True)
-    print(vgg11_layers)
-
-    OUTPUT_DIM = numclasses
-    model = VGG(vgg11_layers, OUTPUT_DIM)
-
-    print(model)
-
-    #pre-trained VGG11 with batch normalization, 140MB
-    #import torchvision.models as models
-    pretrained_model = models.vgg11_bn(pretrained = True)
-
-    print(pretrained_model)
-    #the pre-trained model loaded is exactly the same as the one we have defined with one exception - the output of the final linear layer.
-    #All of torchvision's pre-trained models are trained as image classification models on the ImageNet dataset. 
-    #A dataset of 224x224 color images with 1000 classes, therefore the final layer will have a 1000 dimensional output.
-    #We can get the last layer specifically by indexing into the classifier layer of the pre-trained model.
-    print("VGG last layer size:", pretrained_model.classifier[-1])
-    #We'll define a new final linear layer which has to have an input size equal to that of the layer we are replacing - as it's input will be the 4096 dimensional output from the previous linear layer in the classifier.
-    IN_FEATURES = pretrained_model.classifier[-1].in_features 
-    final_fc = nn.Linear(IN_FEATURES, OUTPUT_DIM) #final_fc will be initialized randomly. It is the only part of our model with its parameters not pre-trained.
-    pretrained_model.classifier[-1] = final_fc
-    print(pretrained_model.classifier)
-
-    #We can load the parameters of the pretrained_model into our model by loading the parameters (state_dict) from the pretrained_model into our model
-    #This is only possible as our model has the exact same layers (order and shape) as the pretrained_model with the final linear layer replaced
-    model.load_state_dict(pretrained_model.state_dict())
-    num_trainparameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f'The model has {num_trainparameters} trainable parameters')
-
-    #Instead of training all of the parameters we have loaded from a pre-trained model, we could instead only learn some of them and leave some "frozen" at their pre-trained values. 
-    for parameter in model.features.parameters():
-        parameter.requires_grad = False
-    #We could also freeze the classifier layer, however we always want to train the last layer
-    for parameter in model.classifier[:-1].parameters():
-        parameter.requires_grad = False
-
 def get_vgg_layers(config, batch_norm):
-
-    #Batch normalization: BN is a layer with learnable parameters - two per filter denoted  γ  and  β
-    #We first calculate the mean and variance across each channel dimension of the batch
-    #Then normalize the batch by subtracting the channel means and dividing by the channel stds
-    #We then scale and shift each channel of this normalized batch of inputs,  x^i  using  γ  and  β
-    #there is a better mean and std for our task instead of zero and one. If this is the case then our model can learn this whilst training as  γ  and  β  are learnable parameters
-
     layers = []
     in_channels = 3
-    
+
     for c in config:
-        assert c == 'M' or isinstance(c, int)
         if c == 'M':
-            layers += [nn.MaxPool2d(kernel_size = 2)]
+            layers += [nn.MaxPool2d(kernel_size=2)]
         else:
-            conv2d = nn.Conv2d(in_channels, c, kernel_size = 3, padding = 1)
+            conv2d = nn.Conv2d(in_channels, c, kernel_size=3, padding=1)
             if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(c), nn.ReLU(inplace = True)]
+                layers += [conv2d, nn.BatchNorm2d(c), nn.ReLU(inplace=True)]
             else:
-                layers += [conv2d, nn.ReLU(inplace = True)]
+                layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = c
-            
+
     return nn.Sequential(*layers)
+
+def create_vggcustommodel(num_classes, config, batch_norm=True):
+    # Create VGG layers based on the provided config and whether to include batch normalization
+    features = get_vgg_layers(config, batch_norm=batch_norm)
+    model = nn.Sequential(
+        features,
+        nn.AdaptiveAvgPool2d(output_size=(7, 7)),
+        nn.Flatten(),
+        nn.Linear(512 * 7 * 7, 4096),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.5),
+        nn.Linear(4096, 4096),
+        nn.ReLU(inplace=True),
+        nn.Dropout(0.5),
+        nn.Linear(4096, num_classes)
+    )
+
+    print(model)
+    return model
+
 
 class CNNNet1(nn.Module): #32*32 image input
     def __init__(self, numclasses):
